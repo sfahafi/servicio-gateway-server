ebaa9158496a9eb9bea5417999ac3c5e881d3337
ubuntu <ubuntu@ubuntu-Lenovo-B40-70>
2022-03-16 18:53:34 -0300

    primer commit

---
+++ src/test/java/com/sfahafi/app/gateway/ServicioGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfahafi.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
