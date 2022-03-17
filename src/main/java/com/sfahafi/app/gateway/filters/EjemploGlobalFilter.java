13ca63262edd5c4f6e7df3e3615631f068da95a7
ubuntu <ubuntu@ubuntu-Lenovo-B40-70>
2022-03-16 23:48:55 -0300

    Implementando filtro globales pre y post

---
+++ src/main/java/com/sfahafi/app/gateway/filters/EjemploGlobalFilter.java
@@ -0,0 +1,30 @@
+package com.sfahafi.app.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class EjemploGlobalFilter implements GlobalFilter{
+
+	private final Logger logger = LoggerFactory.getLogger(EjemploGlobalFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("ejecutando filtro pre");
+		
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			logger.info("ejecutando filtro post");
+			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		}));
+	}
+
+}
